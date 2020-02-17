3b97e8e9c66a3bcc9d9ab77f687dd8fc56f6f0e1
rafasaurus <rafa.grigorian@gmail.com>
2020-02-17 11:38:09 +0400

    Refactor, memory management on watch stream

---
+++ watch/src/main/java/visight/comVisight/watch/MainActivity.java
@@ -0,0 +1,294 @@
+package visight.comVisight.watch;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Button listen, send,
+    Button listDevices;
+    ListView listView;
+    TextView msg_box, status;
+    EditText writeMsg;
+
+    BluetoothAdapter bluetoothAdapter;
+    BluetoothDevice[] btArray;
+
+    SendRecieve sendRecieve;
+
+    static final int STATE_LISTENING = 1;
+    static final int STATE_CONNECTING = 2;
+    static final int STATE_CONNECTED = 3;
+    static final int STATE_CONNECTION_FAILED = 4;
+    static final int STATE_MESSAGE_RECEIVED = 5;
+
+    int REQUEST_ENABLE_BLUETOOTH = 1;
+
+    private static final String APP_NAME ="visight_watch";
+    private static final UUID MY_UUID = UUID.fromString("c52a2a66-381b-468a-8fa0-b651b29021b4");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewByIdes();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(!bluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent,REQUEST_ENABLE_BLUETOOTH);
+            Intent intent2 = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            intent2.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,120);
+            startActivity(intent2);
+        }
+
+        implementListeners();
+    }
+
+    private void implementListeners(){
+        listDevices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Set<BluetoothDevice> bt = bluetoothAdapter.getBondedDevices();
+                String[] strings = new String[bt.size()];
+                btArray = new BluetoothDevice[bt.size()];
+                int index = 0;
+
+                if(bt.size()>0){
+                    for(BluetoothDevice device : bt){
+                        btArray[index] = device;
+                        strings[index] = device.getName();
+                        if (Objects.equals(new String("overkill"), device.getName())) {
+                            ClientClass clientClass = new ClientClass(btArray[index]);
+                            clientClass.start();
+
+                            status.setText("debug");
+                            return;
+                        }
+                        index++;
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_list_item_1,strings);
+                    listView.setAdapter(arrayAdapter);
+                }
+            }
+        });
+
+        // listen.setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //     public void onClick(View v) {
+        //         ServerClass serverClass = new ServerClass();
+        //         serverClass.start();
+        //     }
+        // });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ClientClass clientClass = new ClientClass(btArray[position]);
+                clientClass.start();
+
+                status.setText("Connecting");
+            }
+        });
+
+        // send.setOnClickListener(new View.OnClickListener() {
+        //     @Override
+        //     public void onClick(View v) {
+        //         String string = String.valueOf(writeMsg.getText());
+        //         sendRecieve.write(string.getBytes());
+        //     }
+        // });
+    }
+    Handler handler = new Handler(new Handler.Callback() {
+        @Override
+        public boolean handleMessage(Message msg) {
+            switch (msg.what){
+                case STATE_LISTENING:
+                    status.setText("Listening");
+                    break;
+                case STATE_CONNECTING:
+                    status.setText("Connecting");
+                    break;
+                case STATE_CONNECTED:
+                    status.setText("Connected");
+                    break;
+                case STATE_CONNECTION_FAILED:
+                    status.setText("Connection Failed");
+                    break;
+                case STATE_MESSAGE_RECEIVED:
+                    byte[] readBuff = (byte[]) msg.obj;
+                    String tempMsg = new String(readBuff,0,msg.arg1);
+                    msg_box.setText(tempMsg);
+                    break;
+            }
+            return true;
+        }
+    });
+    private void findViewByIdes() {
+        // listen = (Button) findViewById(R.id.listen);
+        // send = (Button) findViewById(R.id.send);
+        listView = (ListView) findViewById(R.id.listView);
+        // msg_box = (TextView) findViewById(R.id.msg);
+        status = (TextView) findViewById(R.id.status);
+        // writeMsg = (EditText) findViewById(R.id.writemsg);
+        listDevices = (Button) findViewById(R.id.listDevices);
+
+    }
+    private class ServerClass extends Thread{
+        private BluetoothServerSocket serverSocket;
+
+        public ServerClass(){
+            try {
+                serverSocket=bluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(APP_NAME,MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        public void run(){
+            BluetoothSocket socket = null;
+
+            while (socket==null){
+                try {
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTING;
+                    handler.sendMessage(message);
+
+                    socket=serverSocket.accept();
+                }catch (IOException e){
+                    e.printStackTrace();
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTION_FAILED;
+                    handler.sendMessage(message);
+                }
+
+                if(socket!=null){
+                    Message message = Message.obtain();
+                    message.what = STATE_CONNECTED;
+                    handler.sendMessage(message);
+
+                    //Code for send / receive
+                    sendRecieve = new SendRecieve(socket);
+                    sendRecieve.start();
+                    break;
+                }
+            }
+        }
+    }
+
+    private class ClientClass extends Thread{
+        private BluetoothDevice device;
+        private BluetoothSocket socket;
+
+        public ClientClass (BluetoothDevice device1){
+            device = device1;
+
+            try {
+                socket = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void run(){
+
+            try {
+                socket.connect();
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTED;
+                handler.sendMessage(message);
+
+                sendRecieve = new SendRecieve(socket);
+                sendRecieve.start();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Message message = Message.obtain();
+                message.what = STATE_CONNECTION_FAILED;
+                handler.sendMessage(message);
+            }
+        }
+    }
+
+    private class SendRecieve extends Thread{
+        private final BluetoothSocket bluetoothSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public SendRecieve(BluetoothSocket socket){
+            bluetoothSocket = socket;
+            InputStream tempIn = null;
+            OutputStream tempOut = null;
+
+            try {
+                tempIn = bluetoothSocket.getInputStream();
+                tempOut = bluetoothSocket.getOutputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            inputStream = tempIn;
+            outputStream = tempOut;
+        }
+        public void run(){
+            byte[] buffer = new byte[6];
+            int buffer_length = 0;
+            FileInputStream fileStream = null;
+            try {
+                fileStream = new FileInputStream("/sys/class/misc/fastacc_mpu/device/fifo");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            while (true){
+                try {
+                    buffer_length = fileStream.read(buffer);
+                    if (buffer_length == 6)
+                    {
+                        outputStream.write(buffer);
+                        try {
+                            TimeUnit.MICROSECONDS.sleep(1;
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        public void write(byte[] bytes){
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
